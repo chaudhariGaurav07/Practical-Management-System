ea8254cdadd0b687989a688fe4caa2903fcb79a5
chaudhariGaurav07 <gauravchaudhari7717@gmail.com>
2024-11-20 17:01:29 +0530

    add teacher and get users

--- controller/userController.js
+++ controller/userController.js
@@ -21,3 +21,62 @@ export const createAdmin = async(req,res) => {
          error:"error occured"
     }
 }
+
+export const createTeacher = async (req,res)=> {
+    try {
+         const {name, email, password}=req.body;
+
+         const user = new User({
+         name,
+         email,
+         password,
+         role: "Teacher"
+    })
+   const saveUser = await user.save();
+
+   res.json({
+      saveUser,
+      message:"Teacher connected succesfully"
+   })
+    } catch (error) {
+        error:"error occured"
+    }
+}
+
+export const createStudent = async (req,res)=> {
+    try {
+         const {name, email, password}=req.body;
+
+         const user = new User({
+         name,
+         email,
+         password,
+         role: "Student"
+    })
+   const saveUser = await user.save();
+
+   res.json({
+      saveUser,
+      message:"student connected succesfully"
+   })
+    } catch (error) {
+        error:"error occured"
+    }
+}
+
+
+
+
+export const getAllUsers = async(req,res)=>{
+    try {
+      const getusers=await User.find()
+      res.json({
+        getusers
+      })
+    } catch (error) {
+      res.json({
+        error:"Cannot fetch data"
+      })
+      console.log(error)
+    }
+  }
--- routes/feedbackRouts.js
+++ routes/feedbackRouts.js
@@ -1,7 +1,12 @@
-import { createAdmin } from "../controller/userController.js";
+import { createAdmin,createStudent,createTeacher,getAllUsers } from "../controller/userController.js";
 const router = express.Router()
 import express from "express";
 
 router.post('/admin/create',createAdmin)
+router.post('/teacher/create',createTeacher)
+router.post('/student/create',createStudent)
+
+router.get('/user/get',getAllUsers)
+
 
 export default router;
